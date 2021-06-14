87e0ab3fc75e091df8d3399aafa3055f04637961
Blaise Telfer <56236726+Blaise-Telfer@users.noreply.github.com>
2021-06-14 02:36:22 -0700

    Add files via upload

---
+++ frontend/src/components/AddCustomerModal.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import { Modal, Button, Row, Col, Form, Image } from 'react-bootstrap';
+import axios from 'axios';  
+
+
+class AddCustomerModal extends Component{
+    
+    render(){
+        return (
+            <div className="container">
+			
+            </div>
+        )
+    }
+
+}
+
+export default AddCustomerModal;
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,1 +1,30 @@
+import React,{Component} from 'react';
+import {NavLink} from 'react-router-dom';
+import {Navbar, Nav} from 'react-bootstrap';
 
+class Navbar2 extends Component{
+
+    render(){
+        return(
+            <Navbar bg="dark" expand="lg">
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav>
+                <NavLink className="d-inline p-2 bg-dark text-white" to="/">
+                    Home
+                </NavLink>
+                <NavLink className="d-inline p-2 bg-dark text-white" to="/customers">
+                    Customers
+                </NavLink>
+                <NavLink className="d-inline p-2 bg-dark text-white" to="/emails">
+                    Email List
+                </NavLink>
+
+                </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    }
+}
+
+export default Navbar2
