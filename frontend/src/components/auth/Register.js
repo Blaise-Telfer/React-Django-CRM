8461090b2895a612588668110880f4ad8e97f9d0
Blaise Telfer <56236726+Blaise-Telfer@users.noreply.github.com>
2021-06-18 03:31:19 -0700

    Add files via upload

--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -5,13 +5,15 @@ import { Form, FormGroup, Input, Label, Button } from 'reactstrap';
 
 // REDUX
 import { connect } from 'react-redux';
-import { loginUser } from '../../actions/auth';
+import { registerUser } from '../../actions/auth';
 import setAlert from '../../actions/alert';
 
-const Login = ({ isAuthenticated, loginUser, setAlert, history }) => {
+const Register = ({ isAuthenticated, registerUser, setAlert, history }) => {
   const [formData, setFormData] = useState({
     username: '',
-    password: ''
+    email: '',
+    password: '',
+    password2: ''
   });
 
   if (isAuthenticated) history.push('/dashboard');
@@ -20,28 +22,37 @@ const Login = ({ isAuthenticated, loginUser, setAlert, history }) => {
     ...formData,
     [e.target.name]: e.target.value
   });
-
-  const { username, password } = formData;
+  
+  const { username, email, password, password2 } = formData;
 
   const onSubmit = e => {
     e.preventDefault();
 
-    if (!username.trim() || !password.trim()) {
+    if (
+      !username.trim() ||
+      !email.trim() ||
+      !password.trim() ||
+      !password2.trim()
+    ) {
       setAlert('Please fill out all fields!', 400, 'danger');
+    } else if (password !== password2 ) {
+      setAlert('Password does not match!', 400, 'danger');
     } else {
-      loginUser(formData);
+      registerUser(formData);
       setFormData({
         ...formData,
         username: '',
-        password: ''
+        email: '',
+        password: '',
+        password2: ''
       });
     }
   };
 
   return (
-    <div style={{ width: '60%', margin: 'auto' }} className='Login'>
+    <div style={{ width: '60%', margin: 'auto' }} className='Register'>
       <header>
-        <h3 style={{ fontWeight: 'bold' }}>Log In</h3>
+        <h3 style={{ fontWeight: 'bold' }}>Register</h3>
       </header>
       <hr />
       <Form onSubmit={e => onSubmit(e)}>
@@ -58,10 +69,22 @@ const Login = ({ isAuthenticated, loginUser, setAlert, history }) => {
           />
         </FormGroup>
         <FormGroup>
+          <Label htmlFor='email'>Email</Label>
+          <Input
+            id='email'
+            type='email'
+            name='email'
+            value={email}
+            className='form-control'
+            placeholder='Enter email'
+            onChange={e => onChange(e)}
+          />
+        </FormGroup>
+        <FormGroup>
           <Label htmlFor='password'>Password</Label>
           <Input
             id='password'
-            type='password'
+            type='text'
             name='password'
             value={password}
             className='form-control'
@@ -69,19 +92,31 @@ const Login = ({ isAuthenticated, loginUser, setAlert, history }) => {
             onChange={e => onChange(e)}
           />
         </FormGroup>
-        <Button type='submit' color='primary'>Login</Button>
+        <FormGroup>
+          <Label htmlFor='password2'>Confirm password</Label>
+          <Input
+            id='password2'
+            type='password'
+            name='password2'
+            value={password2}
+            className='form-control'
+            placeholder='Confirm password'
+            onChange={e => onChange(e)}
+          />
+        </FormGroup>
+        <Button type='submit' color='primary'>Register</Button>
       </Form>
       <br/>
       <p className='text-muted text-center'>
-        If you don't have an account? Let's <Link to='/register'>Register</Link>
+        If you already have an account? Let's <Link to='/login'>Login</Link>
       </p>
     </div>
   )
 }
 
-Login.propTypes = {
+Register.propTypes = {
   isAuthenticated: PropTypes.bool,
-  loginUser: PropTypes.func.isRequired,
+  registerUser: PropTypes.func.isRequired,
   setAlert: PropTypes.func.isRequired
 };
 
@@ -89,4 +124,4 @@ const mapStateToProps = state => ({
   isAuthenticated: state.auth.isAuthenticated
 });
 
-export default connect(mapStateToProps, { loginUser, setAlert })(Login);
+export default connect(mapStateToProps, { registerUser, setAlert })(Register);
