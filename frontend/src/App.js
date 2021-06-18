84a909ef0a931f35bcabc54034a976cb18bf2f7a
Blaise Telfer <56236726+Blaise-Telfer@users.noreply.github.com>
2021-06-18 03:25:31 -0700

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,68 @@
+import React, { Fragment, useEffect } from 'react';
+import { 
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import { Container } from 'reactstrap';
 import './App.css';
-import Home from './Home';
-import Customers from './components/Customers';
-import Navbar2 from './components/Navbar';
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// REDUX IMPORTS
+import { Provider } from 'react-redux';
+import Store from './store';
+import { loadUser } from './actions/auth';
 
-function App() {
+// COMPONENTS
+import AppHeader from './layouts/AppHeader';
+import Home from './components/Home';
+import Dashboard from './components/Dashboard';
+import Lead from './components/Lead';
+import LeadDetail from './components/LeadDetail';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
+import setAuthToken from './utils/setAuthToken';
+
+// PRIVATE ROUTE & ALERT
+import PrivateRoute from './components/routing/PrivateRoute';
+import Alert from './layouts/Alert';
+
+if (localStorage.token) setAuthToken(localStorage.token);
+
+const App = () => {
+  useEffect(() => {
+    Store.dispatch(loadUser());
+  }, []);
+
   return (
-    <BrowserRouter>
-    <div className="container">
-     <h3 className="m-3 d-flex justify-content-center">
-       React JS Tutorial
-     </h3>
-	 
-	 <Navbar2/>
-	 
-     <Switch>
-       <Route path='/' component={Home} exact/>
-       <Route path='/customers' component={Customers}/>
-     </Switch>
-    </div>
-    </BrowserRouter>
+    <Provider store={Store}>
+    <Router>
+      <Fragment>
+        <header>
+          <AppHeader />
+        </header>
+        <main className='my-5'>
+          <Alert />
+          <Switch>
+            <Container>
+              <Route exact path='/' component={Home} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute exact path='/leads' component={Lead} />
+              <PrivateRoute exact path='/leads/:id' component={LeadDetail} />
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+            </Container>
+          </Switch>
+        </main>
+        <hr />
+        <footer>
+          <p className='text-center lead text-muted'>
+            <strong>Lead Manager </strong> &copy; By Ruzny MA
+          </p>
+        </footer>
+      </Fragment>
+    </Router>
+    </Provider>
   );
 }
 
