171e16715bc28c5c851f6461cbf1a22ead8bf3d7
Blaise Telfer <56236726+Blaise-Telfer@users.noreply.github.com>
2021-06-18 03:30:43 -0700

    Add files via upload

--- frontend/src/components/routing/PrivateRoute.js
+++ frontend/src/components/routing/PrivateRoute.js
@@ -1,1 +1,38 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
+const PrivateRoute = ({
+  component: Component,
+  auth: { 
+    loading,
+    isAuthenticated
+  },
+  ...rest
+}) => {
+  return (
+    <Route
+      render={props =>{
+        if (loading) {
+          return <h1>Loading</h1>
+        } else if (!isAuthenticated) {
+          return <Redirect to='/login' />
+        } else {
+          return <Component {...props} />
+        }
+      }}
+      {...rest}
+    />
+  );
+}
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
