430236ff5d99741538017bd21290339343a74284
Blaise Telfer <56236726+Blaise-Telfer@users.noreply.github.com>
2021-06-18 03:19:01 -0700

    Delete backend/crm directory

--- backend/crm/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from . import views
-
-urlpatterns = [
-	path('department/', views.departmentApi, name="department"),
-	path('customer/', views.employeeApi, name="customer")
-	
-]
